04ab8b36186a7a2a2e7d420725f5b37feffbd3f2
sacchida <107237945+sacchida@users.noreply.github.com>
2022-11-22 14:54:35 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -36,11 +36,11 @@ const Main = () => {
                         'slug': item.attributes.art_slug,
                         'descr': item.attributes.art_descr === null ? " " : item.attributes.art_descr,
                         'date': item.attributes.createdAt,
-                        'img': "https://dunews.herokuapp.com" + item.attributes.art_img.data.attributes.url,
+                        'img': "https://dunews.herokuapp.com" + (item.attributes.art_img.data.attributes.url === null ? "" : item.attributes.art_img.data.attributes.url),
                         'category': item.attributes.category.data === null ? "All" : item.attributes.category.data.attributes.ctgy_name,
                         'category_slug': item.attributes.category.data === null ? "all" : item.attributes.category.data.attributes.ctgy_slug,
-                        'writer': item.attributes.writer.data.attributes.wr_name,
-                        'writer_photo': "https://dunews.herokuapp.com" + item.attributes.writer.data.attributes.wr_photo.data.attributes.url,
+                        'writer': item.attributes.writer.data.attributes.wr_name === null ? "NONE" : item.attributes.writer.data.attributes.wr_name,
+                        'writer_photo': "https://dunews.herokuapp.com" + (item.attributes.writer.data.attributes.wr_photo.data.attributes.url === null ? "" : item.attributes.writer.data.attributes.wr_photo.data.attributes.url),
                     };
                 });
                 setData(temp);
